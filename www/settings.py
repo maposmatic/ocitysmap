3b58bd825ac87c03b38de204f67e85bfea118636
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2009-08-28 15:42:41 +0200

    Split settings into settings_local

--- www/settings.py
+++ www/settings.py
@@ -1,20 +1,10 @@
 # Django settings for www project.
 
-DEBUG = True
+from settings_local import *
+
 TEMPLATE_DEBUG = DEBUG
 
-ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
-)
-
 MANAGERS = ADMINS
-
-DATABASE_ENGINE = 'postgresql_psycopg2'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'testdb'             # Or path to database file if using sqlite3.
-DATABASE_USER = 'test'             # Not used with sqlite3.
-DATABASE_PASSWORD = 'test'         # Not used with sqlite3.
-DATABASE_HOST = 'localhost'             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
