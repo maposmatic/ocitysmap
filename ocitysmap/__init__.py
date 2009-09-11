18ee95eb5874093aafb77224ee41de94b12edb52
David Decotigny <d2@maposmatic.org>
2009-09-11 23:26:19 +0200

    Don't implicitely import most packages from the __init__.py
    This patch removes almost everything from the __init__, since we don't
    want to merge symbols from submodules into the package. That way, we
    don't implicitely import almost all submodules when we import the
    package. This allows to use some ocitysmap submodules from outside
    ocitysmap without having to provide osgeo or mapnik support.

--- ocitysmap/__init__.py
+++ ocitysmap/__init__.py
@@ -28,8 +28,3 @@ Provide documentation here.
 
 __author__ = 'The Hackfest2009 team'
 __version__ = '0.1'
-
-# Package symbols
-from street_index import OCitySMap, BaseOCitySMapError, UnsufficientDataError
-from coords import BoundingBox
-from grid import GridDescriptor
