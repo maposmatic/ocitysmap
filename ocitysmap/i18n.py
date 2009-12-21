afe67d18ecf03206b69088047009e3ede82eb088
David MENTRE <dmentre@linux-france.org>
2009-12-21 23:59:16 +0100

    Add basic support for all English-speaking countries

--- ocitysmap/i18n.py
+++ ocitysmap/i18n.py
@@ -121,12 +121,27 @@ class i18n_generic(i18n):
 language_map = { # 'code_CODE.UTF-8': i18n_template_code_CODE(), # example for
                                                                  # new language
                  'C': i18n_generic('C'),
+                 'fr_BE.UTF-8': i18n_fr_generic('fr_BE.UTF-8'),
                  'fr_FR.UTF-8': i18n_fr_generic('fr_FR.UTF-8'),
                  'fr_CA.UTF-8': i18n_fr_generic('fr_CA.UTF-8'),
                  'fr_CH.UTF-8': i18n_fr_generic('fr_CH.UTF-8'),
                  'fr_LU.UTF-8': i18n_fr_generic('fr_LU.UTF-8'),
+                 'en_AG': i18n_generic('en_AG'),
+                 'en_AU.UTF-8': i18n_generic('en_AU.UTF-8'),
+                 'en_BW.UTF-8': i18n_generic('en_BW.UTF-8'),
+                 'en_CA.UTF-8': i18n_generic('en_CA.UTF-8'),
+                 'en_DK.UTF-8': i18n_generic('en_DK.UTF-8'),
                  'en_GB.UTF-8': i18n_generic('en_GB.UTF-8'),
-                 'fr_BE.UTF-8': i18n_fr_generic('fr_BE.UTF-8'),
+                 'en_HK.UTF-8': i18n_generic('en_HK.UTF-8'),
+                 'en_IE.UTF-8': i18n_generic('en_IE.UTF-8'),
+                 'en_IN': i18n_generic('en_IN'),
+                 'en_NG': i18n_generic('en_NG'),
+                 'en_NZ.UTF-8': i18n_generic('en_NZ.UTF-8'),
+                 'en_PH.UTF-8': i18n_generic('en_PH.UTF-8'),
+                 'en_SG.UTF-8': i18n_generic('en_SG.UTF-8'),
+                 'en_US.UTF-8': i18n_generic('en_US.UTF-8'),
+                 'en_ZA.UTF-8': i18n_generic('en_ZA.UTF-8'),
+                 'en_ZW.UTF-8': i18n_generic('en_ZW.UTF-8'),
                  'de_BE.UTF-8': i18n_generic('de_BE.UTF-8'),
                  'nl_BE.UTF-8': i18n_generic('nl_BE.UTF-8')}
 
