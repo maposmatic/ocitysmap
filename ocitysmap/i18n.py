4d3a32533f533372fd202c3bf8bcdc6bf223bb72
Łukasz Jernaś <deejay1@srem.org>
2010-02-08 14:09:41 +0100

    Initial Polish language support.
    This patch adds support for some of the caveats in maps of Poland
    generated from ocitysmap. Probably more will be needed.
    
    Signed-off-by: Maxime Petazzoni <maxime.petazzoni@bulix.org>

--- ocitysmap/i18n.py
+++ ocitysmap/i18n.py
@@ -577,6 +577,57 @@ class i18n_hr_HR(i18n):
            e.g. É and E are equals in French map index"""
         return self._upper_unaccent_string(a) == self._upper_unaccent_string(b)
 
+class i18n_pl_generic(i18n):
+ 
+    APPELLATIONS = [ u"Dr.", u"Doktora", u"Ks.", u"Księdza",
+                     u"Generała", u"Gen.",
+                     u"Aleja", u"Plac", u"Pl.",
+                     u"Rondo", u"rondo", u"Profesora",
+                     u"Prof.",
+                     u"" ]
+
+    DETERMINANTS = [ u"\s?im.", u"\s?imienia", u"\s?pw.",
+                     u"" ]
+    
+    SPACE_REDUCE = re.compile(r"\s+")
+    PREFIX_REGEXP = re.compile(r"^(?P<prefix>(%s)(%s)?)\s?\b(?P<name>.+)" %
+                                    ("|".join(APPELLATIONS),
+                                     "|".join(DETERMINANTS)),
+                                      re.IGNORECASE | re.UNICODE)
+
+
+    def __init__(self, language, locale_path):
+        self.language = str(language)
+        _install_language(language, locale_path)
+
+    def language_code(self):
+        return self.language
+
+    def user_readable_street(self, name):
+        #
+        # Make sure name actually contains something,
+        # the PREFIX_REGEXP.match fails on zero-length strings
+        #
+        if len(name) == 0:
+            return name
+
+        name = name.strip()
+        name = self.SPACE_REDUCE.sub(" ", name)
+        matches = self.PREFIX_REGEXP.match(name)
+        #
+        # If no prefix was captured, that's okay. Don't substitute
+        # the name however, "<name> ()" looks silly
+        #
+        if matches == None:
+            return name
+
+        if matches.group('prefix'):
+            name = self.PREFIX_REGEXP.sub(r"\g<name>, \g<prefix>", name)
+        return name
+
+    def first_letter_equal(self, a, b):
+        return a == b
+
 
 class i18n_generic(i18n):
     def __init__(self, language, locale_path):
@@ -651,6 +702,7 @@ language_class_map = {
     'ar_YE.UTF-8': i18n_ar_generic,
     'hr_HR.UTF-8': i18n_hr_HR,
     'ru_RU.UTF-8': i18n_ru_generic,
+    'pl_PL.UTF-8': i18n_pl_generic,
 }
 
 def install_translation(locale_name, locale_path):
