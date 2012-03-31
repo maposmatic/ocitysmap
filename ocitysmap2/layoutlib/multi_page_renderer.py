d3fdeffb218090a77e0d0dfbda914303accb9548
Thomas Petazzoni <thomas.petazzoni@enix.org>
2012-03-31 09:46:16 +0200

    multi-page: update copyright header
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@enix.org>

--- ocitysmap2/layoutlib/multi_page_renderer.py
+++ ocitysmap2/layoutlib/multi_page_renderer.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
 
 # ocitysmap, city map and street index generator from OpenStreetMap data
-# Copyright (C) 2010  David Decotigny
-# Copyright (C) 2010  Frédéric Lehobey
-# Copyright (C) 2010  Pierre Mauduit
-# Copyright (C) 2010  David Mentré
-# Copyright (C) 2010  Maxime Petazzoni
-# Copyright (C) 2010  Thomas Petazzoni
-# Copyright (C) 2010  Gaël Utard
+# Copyright (C) 2012  David Mentré
+# Copyright (C) 2012  Thomas Petazzoni
+# Copyright (C) 2012  Gaël Utard
+# Copyright (C) 2012  Étienne Loks
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
