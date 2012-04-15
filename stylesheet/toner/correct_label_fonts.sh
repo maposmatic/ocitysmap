2c5aa73c8aa194a55c108dcbe56db7990841b1d8
Étienne Loks <etienne.loks@peacefrogs.net>
2012-04-15 16:32:17 +0200

    Stamen Toner stylesheet: create style files, fix locality, hamlet and village names
    As the changes in the base stylesheet begins to be heavy, specific
    stylesheets files have been created. Reference files (labels.mms for
    labels-ocitysmap.mss and style-toner-standard.mml for
    style-toner-ocitysmap.mml) have been added to ocitysmap repository to
    track changes on the toner main repository.
    
    style-toner-ocitysmap.mml and labels-ocitysmap.mss have been updated
    to display names of localities, hamlets and villages.
    
    INSTALL instructions have been updated.

--- stylesheet/toner/correct_label_fonts.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-sed -i 's/Italic/Oblique/' labels.mss
-sed -i 's/Arial Unicode MS Regular/DejaVu Sans/' labels.mss
-sed -i 's/Arial Regular/DejaVu Sans/' labels.mss
-sed -i 's/Arial Unicode MS/DejaVu Sans/' labels.mss
-sed -i 's/Arial Regular/DejaVu/' labels.mss
-sed -i 's/Arial Bold/DejaVu Sans Bold/' labels.mss
